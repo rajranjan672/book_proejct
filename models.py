2faecef849e9c894d16e2e8a740946a6b399483b
rajranjan672 <76998014+rajranjan672@users.noreply.github.com>
2021-07-13 13:54:54 +0530

    Delete models.py

--- models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Books(models.Model):
-    title =models.CharField(max_length=100)
-    author =models.CharField(max_length=100)
-    language =models.CharField(max_length=100)
